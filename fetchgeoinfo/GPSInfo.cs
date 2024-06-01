95ef68e5fb65097959ae43fd27ede8bd01fddd5e
Vigil1Light <zerry319.wit@gmail.com>
2024-06-01 23:54:06 +0800

    Add project files.

---
+++ fetchgeoinfo/GPSInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fetchgeoinfo
+{
+    public class GPSInfo
+    {
+        public string version {  get; set; }
+        public string latref { get; set; }
+        public string lonref { get; set; }
+        public string lat {  get; set; }
+        public string lon { get; set; }
+        public string altref { get; set; }
+        public string alt { get; set; }
+    }
+}
---
+++ fetchgeoinfo/Program.cs
@@ -0,0 +1,61 @@
+﻿using MetadataExtractor;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace fetchgeoinfo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string curDic = System.IO.Directory.GetCurrentDirectory() + "\\images";
+            try
+            {
+                string[] files = System.IO.Directory.GetFiles(curDic);
+                GPSInfo gpsInfo = new GPSInfo();
+                if(File.Exists("result.txt"))
+                {
+                    File.Delete("result.txt");
+                }
+                var result = File.Create("result.txt");
+                result.Close();
+                foreach (string file in files)
+                {
+                    var directories = ImageMetadataReader.ReadMetadata(file);
+                    foreach (var directory in directories)
+                    {
+                        if(directory.Name == "GPS")
+                        {
+                            foreach (var tag in directory.Tags)
+                            {
+                                if (tag.Name == "GPS Version ID")
+                                    gpsInfo.version = tag.Description;
+                                if (tag.Name == "GPS Latitude Ref")
+                                    gpsInfo.latref = tag.Description;
+                                if(tag.Name == "GPS Latitude")
+                                    gpsInfo.lat = tag.Description;
+                                if(tag.Name == "GPS Longitude Ref")
+                                    gpsInfo.lonref = tag.Description;
+                                if(tag.Name == "GPS Longitude")
+                                    gpsInfo.lon = tag.Description;
+                                if(tag.Name == "GPS Altitude Ref")
+                                    gpsInfo.altref = tag.Description;
+                                if(tag.Name == "GPS Altitude")
+                                    gpsInfo.alt = tag.Description;
+                            }
+                        }  
+                    }
+                    File.AppendAllText(@"result.txt", Path.GetFileName(file) + ":" + gpsInfo.lat + "," + gpsInfo.lon + "," + gpsInfo.alt + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.WriteLine("successfully done");
+            Console.ReadKey();
+        }
+    }
+}
